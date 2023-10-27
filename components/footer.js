f84a65d3554ce02e46d3b5964c2cfe28f9b3f6c2
aniketSingh0310 <aniketreuls@gmail.com>
2023-10-27 12:47:17 +0530

    .

--- components/footer.js
+++ components/footer.js
@@ -21,9 +21,9 @@ const Footer = () => {
   
   <nav>
     <header className="footer-title">Contact Us </header> 
-    <a className="link link-hover flex items-center gap-2 text-base"><HiMail className="text-[#42A5FF]"/>rajvarma@theserendib.com</a> 
-    <a className="link link-hover flex items-center gap-2 text-base"><FaPhoneAlt className="text-[#42A5FF]"/>+919920811288</a> 
-    <a className="link link-hover flex items-baseline gap-2 text-base"><MdLocationOn className="text-[#42A5FF]"/>Serendib Trading & Properties LLP<br/>
+    <a className="link link-hover flex items-center gap-2 text-base"><HiMail className="text-[#42A5FF] w-[20px] h-[30px]"/>rajvarma@theserendib.com</a> 
+    <a className="link link-hover flex items-center gap-2 text-base"><FaPhoneAlt className="w-[20px] h-[15px] text-[#42A5FF]"/>+919920811288</a> 
+    <a className="link link-hover flex items-baseline gap-2 text-base"><MdLocationOn className=" w-[20px] h-[25px] text-[#42A5FF]"/>Serendib Trading & Properties LLP<br/>
 C-154, 15th Floor, Kalpataru Sparkle, Plot No. C C 23/201,<br/> N. Dharmadikari Road, Bandra Mumbai City Maharashtra 400051
 </a> 
     
