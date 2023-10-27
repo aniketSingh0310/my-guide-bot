99c889ae73af5b8378481dfae6f30b80d627b943
aniketSingh0310 <aniketreuls@gmail.com>
2023-10-27 17:18:16 +0530

    title

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Footer from '@/components/footer'
 import Hero from '@/components/hero'
 import Navbar from '@/components/navbar'
 import NewNav from '@/components/newNav'
+import Head from 'next/head'
 import Image from 'next/image'
 
 
@@ -10,6 +11,15 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
+      <Head>
+        <title>MyGuideBot</title>
+        <meta
+          name="description"
+          content="MGB serves as your passport to unforgettable travel experiences. We make it effortless to discover, plan, and book cherished experiences,
+          encompassing sightseeing, tours, food and beverages, adventure activities, shopping, and nightlife."
+        />
+        {/* Add more meta tags or links to stylesheets/fonts as needed */}
+      </Head>
       <NewNav/>
     <div className="flex-1">
       <Hero/>
