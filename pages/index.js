98c82e212cfc481027d5cae0720d7dd311a45ac2
aniketSingh0310 <aniketreuls@gmail.com>
2023-10-27 16:08:52 +0530

    removed text

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,7 @@ export default function Home() {
       <Navbar/>
     <div className="flex-1">
       <Hero/>
-      {/* Your page content goes here */}
-      <h1 className="text-3xl font-bold mb-4">Welcome to My Next.js Project</h1>
-      <p>This is the main content of your home page.</p>
+      
     </div>
     <Footer />
   </div>
